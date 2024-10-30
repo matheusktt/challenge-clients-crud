8b47b2ffe7ce7b7a4b15c072518d602d1fc175bf
matheusktt <matheus_0119@outlook.com>
2024-10-29 22:48:11 -0300

    first commit

---
+++ src/main/java/com/devsuperior/challenge_clients_crud/ChallengeClientsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.challenge_clients_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeClientsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeClientsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/challenge_clients_crud/ChallengeClientsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.challenge_clients_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeClientsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
